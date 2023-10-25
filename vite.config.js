a290c915b2417939f30fad2049939c257e842397
playolaizq <pol944li@gmail.com>
2023-10-25 16:45:27 +0200

    fix: set vitejs 'base' config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const REPOSITORY_NAME = 'svg-zoom-pan-pinch';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${REPOSITORY_NAME}/`,
 });
