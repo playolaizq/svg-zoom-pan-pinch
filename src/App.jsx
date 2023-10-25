28ee2f7f529aca21265477d8cb9b2a79476d3239
playolaizq <pol944li@gmail.com>
2023-10-25 17:10:31 +0200

    feat: usePinch using new pacakge @use-gesture/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import { useGesture } from 'react-use-gesture';
+import { usePinch } from '@use-gesture/react';
 import classes from './App.module.css';
 
 const MIN_ZOOM = 0.5;
@@ -8,20 +8,15 @@ const MAX_ZOOM = 3;
 
 function App() {
   const svgRef = useRef();
-  const [rotation, setRotation] = useState(0);
+  const transform = useRef({ scale: 1, x: 0, y: 0 });
 
-  const bindPinch = useGesture(
-    {
-      onPinch: ({ movement: [scale] }) => {
-        // Calculate the rotation based on the pinch scale
-        const newRotation = rotation + scale * 90;
-        setRotation(newRotation);
-      },
-    },
-    {
-      initial: () => [rotation],
-    },
-  );
+  const pinchHandlers = usePinch(({ delta: [d], offset: [ox, oy] }) => {
+    // Zoom the SVG based on the pinch gesture and adjust for the pinch center
+    transform.current.scale = transform.current.scale + d * 0.01;
+    transform.current.x -= ox * d * 0.01;
+    transform.current.y -= oy * d * 0.01;
+    svgRef.current.style.transform = `translate(${transform.current.x}px, ${transform.current.y}px) scale(${transform.current.scale})`;
+  });
 
   useEffect(() => {
     const svgContainer = d3.select(svgRef.current);
@@ -57,8 +52,7 @@ function App() {
           version="1.2"
           viewBox="0 0 2000 857"
           xmlns="http://www.w3.org/2000/svg"
-          {...bindPinch()}
-          style={{ transform: `rotate(${rotation}deg)` }}
+          {...pinchHandlers()}
         >
           <path
             d="M1383 261.6l1.5 1.8-2.9 0.8-2.4 1.1-5.9 0.8-5.3 1.3-2.4 2.8 1.9 2.7 1.4 3.2-2 2.7 0.8 2.5-0.9 2.3-5.2-0.2 3.1 4.2-3.1 1.7-1.4 3.8 1.1 3.9-1.8 1.8-2.1-0.6-4 0.9-0.2 1.7-4.1 0-2.3 3.7 0.8 5.4-6.6 2.7-3.9-0.6-0.9 1.4-3.4-0.8-5.3 1-9.6-3.3 3.9-5.8-1.1-4.1-4.3-1.1-1.2-4.1-2.7-5.1 1.6-3.5-2.5-1 0.5-4.7 0.6-8 5.9 2.5 3.9-0.9 0.4-2.9 4-0.9 2.6-2-0.2-5.1 4.2-1.3 0.3-2.2 2.9 1.7 1.6 0.2 3 0 4.3 1.4 1.8 0.7 3.4-2 2.1 1.2 0.9-2.9 3.2 0.1 0.6-0.9-0.2-2.6 1.7-2.2 3.3 1.4-0.1 2 1.7 0.3 0.9 5.4 2.7 2.1 1.5-1.4 2.2-0.6 2.5-2.9 3.8 0.5 5.4 0z"
