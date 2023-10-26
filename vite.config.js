385025e8df1251be0a9966f8c24bd4b33e416abd
playolaizq <pol944li@gmail.com>
2023-10-26 09:06:01 +0200

    feat: simplify without svgr and animated.svg

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 const REPOSITORY_NAME = 'svg-zoom-pan-pinch';
 
@@ -8,7 +7,7 @@ const REPOSITORY_NAME = 'svg-zoom-pan-pinch';
  * @see https://vitejs.dev/config/
  */
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   base: mode === 'development' ? '' : `/${REPOSITORY_NAME}/`,
   server: {
     host: true,
